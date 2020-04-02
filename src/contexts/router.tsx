06b7b49fd5c0c0e7e9c417934bc5150f411d2ab8
Anthony Short <antshort@gmail.com>
2020-04-01 21:47:54 -0700

    v0.1.6

--- src/contexts/router.tsx
+++ src/contexts/router.tsx
@@ -1,4 +1,15 @@
-import { NextRouter } from 'next/router';
+import React from 'react';
+import { NextRouter, useRouter } from 'next/router';
 import { createContext } from 'react';
 
 export const RouterContext = createContext<NextRouter | null>(null);
+
+export function RouterProvider(props: {
+  children: React.ReactNode;
+}): JSX.Element {
+  const { children } = props;
+  const router = useRouter();
+  return (
+    <RouterContext.Provider value={router}>{children}</RouterContext.Provider>
+  );
+}
